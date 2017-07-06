62cbfcfd5ea9bc943e11536d40d66060c992e1f1
Conrad <conradpacesa@gmail.com>
2017-07-06 18:25:17 -0400

    URL fix

--- lib/kele.rb
+++ lib/kele.rb
@@ -6,12 +6,12 @@ class Kele
 
   def initialize(email, password)
     @api_url = 'https://www.bloc.io/api/v1'
-    response = self.class.post('https://www.bloc.io/api/v1/sessions', body: {email: email, password: password})
+    response = self.class.post("#{@api_url}/sessions", body: {email: email, password: password})
     @auth_token =  response['auth_token']
   end
 
   def get_me
-    response = self.class.get('https://www.bloc.io/api/v1/users/me', headers: { "authorization" => @auth_token }).to_json
+    response = self.class.get("#{@api_url}/users/me", headers: { "authorization" => @auth_token }).to_json
     @user_data = JSON.parse(response)
   end
 end
